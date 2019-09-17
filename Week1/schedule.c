a9951f6e49f5214bf1f7da38c47c357483e08b25
duongle26 <leduong26298@gmail.com>
2019-09-18 00:26:02 +0700

    Add function process data

--- Week1/schedule.c
+++ Week1/schedule.c
@@ -2,34 +2,92 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct time_table
+typedef struct time_table
 {
     char code[10];
-    char course[20];
+    char course[50];
     int week_day;
     int am_pm;
     int period_start;
     int period_end;
     char week[20];
     char room[10];
-};
+} time_table;
 
-int readFile(char table[100][100][100], char fileName[20]);
+int readFile(char courses[100][100], char fileName[20]);
+void processData(struct time_table *timeTable, char courses[100][100], int size)
+{
+    char *token, course[100][100], detail[100][100];
+    int i, j, k;
+    for (i = 0; i < size; i++, j = 0, k = 1)
+    {
+        // j = 0;
+        // k = 1;
+
+        token = strtok(courses[i], " ");
+        while (token != NULL)
+        {
+            strcpy(course[j++], token);
+            token = strtok(NULL, " ");
+        }
+        // printf("%s\n", course[j - 1]);
+
+        strcpy(timeTable[i].code, course[0]);
+        strcpy(timeTable[i].course, course[1]);
+
+        while (++k != j - 1)
+        {
+            // printf("%s\n", course[k]);
+            strcat(timeTable[i].course, " ");
+            strcat(timeTable[i].course, course[k]);
+        }
+
+        // printf("%s\n", timeTable[i].course);
+        k = 0;
+        token = strtok(courses[j - 1], ",");
+        while (token != NULL)
+        {
+            strcpy(detail[k++], token);
+            token = strtok(NULL, ",");
+        }
+
+        timeTable[i].week_day = detail[1][0];
+        timeTable[i].am_pm = detail[1][1];
+        timeTable[i].period_start = detail[1][2];
+        timeTable[i].period_end = detail[2][2];
+        strcpy(timeTable[i].week, detail[3]);
+        j = 3;
+        while (++j != k - 1)
+        {
+            strcat(timeTable[i].week, ", ");
+            strcat(timeTable[i].week, detail[j]);
+        }
+
+        strcpy(timeTable[i].room, detail[k - 1]);
+    }
+}
 
 int main()
 {
-    char table[100][100][100];
+    char courses[100][100];
     char fileName[] = "schedule.txt";
-    int fileSize;
-    fileSize = readFile(table, fileName);
+    int size;
 
+    size = readFile(courses, fileName);
+    // for (int i = 0; i < size; i++)
+    // {
+    //     printf("%s\n", courses[i]);
+    // }
+    time_table *timeTable = malloc(size * sizeof(time_table));
+    processData(timeTable, courses, size);
+    free(timeTable);
     return 0;
 }
 
-int readFile(char table[100][100][100], char fileName[20])
+int readFile(char courses[100][100], char fileName[20])
 {
     FILE *fptr;
-    int c, i = 0;
+    int c, count = 0, i = 0;
 
     fptr = fopen(fileName, "r");
     if (fptr == NULL)
@@ -39,11 +97,24 @@ int readFile(char table[100][100][100], char fileName[20])
     }
 
     c = fgetc(fptr);
-    while (feof(fptr) == 0)
+    while (!feof(fptr))
     {
-        printf("%c", c);
+        switch (c)
+        {
+        case '\n':
+            break;
+        case ';':
+            i = 0;
+            count++;
+            break;
+        default:
+            courses[count][i] = c;
+            i++;
+            break;
+        }
         c = fgetc(fptr);
     }
+
     fclose(fptr);
-    return i;
+    return count;
 }
